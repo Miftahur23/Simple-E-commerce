29303203393d12c4029a3b191c94f395fa5a86cf
Miftahur23 <miftahur109970@gmail.com>
2022-03-05 01:21:53 +0600

    templating done

---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,33 @@
+<div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu">
+            <div class="nav">
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Home
+                </a>
+
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Customers
+                </a>
+
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Furnitures
+                </a>
+                
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Catagories
+                </a>
+                
+                <a class="nav-link" href="#">
+                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    Orders
+                </a>
+                
+            </div>
+        </div>
+    </nav>
+</div>
